bd07c937c74bef944d8f63ee6a5f1baacfc298c1
Jacob Allen <boat.mon@gmail.com>
2020-05-18 15:56:29 -0400

    redesigning the profolio part

---
+++ src/components/Webprojects.jsx
@@ -0,0 +1,121 @@
+import React from 'react';
+import '../styles/warning.css';
+
+class Art extends React.Component {
+ render() {
+   return (
+     <div className="portfolio">
+       <p>Portfolio</p>
+     </div>
+   </div>
+  </div>
+
+    <div className="projects" id="projects">
+      <div className="responsive">
+        <div className="gallery">
+          <a href="http://www.animefanhelper.com">
+            <img src="images/AnimeFanHelper.png" alt="AnimeFanHelper" width="800" height="400"/>
+          </a>
+          <div className="desc">Anime Fan Helper is a mulltifunction website that has anime related news stories, anime theme games, and your able to search for any anime you want using the Jikan API. I Created it in 2019 and keep updating it.</div>
+        </div>
+      </div>
+      <div className="responsive">
+        <div className="gallery">
+          <a href="https://www.southeastrealtygroup.net/">
+            <img src="images/SE-website.png" alt="southeast-realty-group" width="800" height="400"/>
+          </a>
+          <div className="desc"> I created this site in 2019 for the company Southeast Realty Group Inc.</div>
+        </div>
+      </div>
+      <div className="responsive">
+        <div className="gallery">
+          <a href="https://github.com/Jacobaallen/Ichi.world">
+            <img src="images/Untitle-Travel.png" alt="Untitle-Travel" width="800" height="400"/>
+          </a>
+          <div className="desc">This site current being built. It is a weekend travel app design to help users find things to do on the weekend. I am currently making it in 2019.</div>
+        </div>
+      </div>
+     </div>
+     <div className="row-2">
+       <div className="responsive">
+         <div className="gallery">
+           <a href="https://beat-blender.herokuapp.com/">
+             <img src="images/BeatBleader.png" alt="" width="800" height="400"/>
+           </a>
+           <div className="desc">BeatBleader uses Geolocation and the Spotify API to connect you with other people around you through music. It was my teams final project at Wyncode in 2019</div>
+         </div>
+       </div>
+       <div className="responsive">
+         <div className="gallery">
+           <a href="https://github.com/Jacobaallen/Weekend-Game-2">
+             <img src="images/game.png" alt="game" width="800" height="400"/>
+           </a>
+           <div className="desc">Elder Tales is a game made for the terminal based on the anime "Log Horizon". I created it in 2019.</div>
+         </div>
+       </div>
+       <div className="responsive">
+         <div className="gallery">
+           <a href="https://github.com/rmbeavin/DishWish">
+             <img src="images/dishwish.png" alt="dishwish" width="800" height="400"/>
+           </a>
+           <div className="desc">Dishwish was a team project made with a team at Wyncode. I was a part of. We created it in 2018.</div>
+         </div>
+       </div>
+     </div>
+       <div className="row-3">
+       <div className="responsive">
+         <div className="gallery">
+           <a href="http://www.animefanhelper.com/whackaluffy.html">
+             <img src="images/whackaluffy0.png" alt="whackaluffy" width="800" height="400"/>
+           </a>
+           <div className="desc">Based on the anime One Piece and the game Whack-a-mole comes Whack-a-Luffy. Created in 2019.</div>
+         </div>
+       </div>
+       <div className="responsive">
+         <div className="gallery">
+           <a href="http://www.animefanhelper.com/space-invaders.html">
+             <img src="images/arcade-game-si.png" alt="Space-invaders" width="800" height="400"/>
+           </a>
+           <div className="desc">Based on the most game popular ever made. Created in 2019.</div>
+         </div>
+       </div>
+       <div className="responsive">
+         <div className="gallery">
+           <a href="http://www.animefanhelper.com/cowboybebopspacebattle.html">
+             <img src="images/arcade-game-CBSB.png" alt="Cowboy-Bebop" width="800" height="400"/>
+           </a>
+           <div className="desc">Based on the anime Cowboy Bebop comes a first person shooter game. Created in 2019.</div>
+         </div>
+       </div>
+     </div>
+     <div className="row-3">
+       <div className="responsive">
+         <div className="gallery">
+           <a href="https://tourdeendofsummer.herokuapp.com/index.html">
+             <img src="images/Tourdeendofsummer.png" alt="TourDeEndofSummer" width="800" height="400"/>
+           </a>
+           <div className="desc">A event sited I create using Adobe Dreamweaver in 2016.</div>
+         </div>
+       </div>
+       <div className="responsive">
+         <div className="gallery">
+           <a href="http://garyramey.com/student-work/gra2144c-sp2016/jallen/project2/index.html">
+             <img src="images/ecommerce-site.png" alt="JaKob" width="800" height="400"/>
+           </a>
+           <div className="desc">A mock up ecommerce site I created using Adobe Muse in 2016.</div>
+         </div>
+       </div>
+       <div className="responsive">
+         <div className="gallery">
+           <a href="http://garyramey.com/student-work/gra2144c-sp2016/jallen/project1/index.html">
+             <img src="images/event-site.png" alt="event" width="800" height="400"/>
+           </a>
+           <div className="desc">A mock up event site I created using Adobe Muse in 2016.</div>
+         </div>
+       </div>
+     </div>
+   )
+  }
+ }
+
+export default Webprojects;
