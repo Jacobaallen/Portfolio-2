133115c9188cfb7eec7936e7b00c781afd882e3b
Jacob Allen <boat.mon@gmail.com>
2020-07-24 17:23:46 -0400

    added to newly created gallery

--- src/components/Artarchive.jsx
+++ src/components/Artarchive.jsx
@@ -1,14 +1,423 @@
 import React from 'react';
+import { render } from 'react-dom';
+import Gallery from 'react-grid-gallery';
 import '../styles/art-archive.css';
 
 class Artarchive extends React.Component {
  render() {
    return (
-     <div className="comingsoon">
-       <h1>Coming Soon</h1>
-    </div>
-  )
+     <>
+     <div className="bgimage-2">
+        <div className="transbox">
+          <div className="portfolio">
+            <p>Art & Design</p>
+          </div>
+        </div>
+      </div>
+
+        <div className="row-4">
+          <div className="responsive">
+            <div className="gallery">
+              <div className="flip-card">
+              <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    <img src="images/drawing-6-2020.JPG" alt="draw" width=  "550" height= "300"/>
+                  </div>
+                  <div className="flip-card-back">
+                    <h3>Tomoko and Ucchii on a date.</h3>
+                    <p>2020</p>
+                    <p>Pencil on paper</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="responsive">
+            <div className="gallery">
+              <div className="flip-card">
+              <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    <img src="images/drawing-7-2019.png" alt="draw" width=  "550" height= "300"/>
+                  </div>
+                  <div className="flip-card-back">
+                    <h3>Concept T shirt design enchanting for Anime Fan Helper.</h3>
+                    <p>2019</p>
+                    <p>Digially drawn on Ipad in Adobe Draw, edited in photoshop and photo taken by me.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="responsive">
+            <div className="gallery">
+              <div className="flip-card">
+                <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    <img src="images/drawing-4-2019.JPG" alt="draw" width=  "450" height= "600"/>
+                  </div>
+                  <div className="flip-card-back">
+                    <h3>Usopp thinking of what to draw.</h3>
+                    <p>2019</p>
+                    <p>Pencil on paper</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+         <div className="row-4">
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+                 <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/guitar-and-cand-2019.jpg" alt="guitar" width=  "400" height= "600"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>Soul King Brook from One Piece Shark Guitar and his Cane</h3>
+                     <p>2019</p>
+                    <p>1/2 inch foam board, Paint, Cardboard, metal cane</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+           </div>
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+                 <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/drawing-3-2018.png" alt="draw" width=  "400" height= "600"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>Anime Mascot Celebration.</h3>
+                     <p>2017/2018</p>
+                    <p>Pencil on paper then digially colored in photoshop and photo taken by me too.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+           </div>
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+                 <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/drawing-2-2018.png" alt="draw" width=  "500" height= "300"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>Luffy catching air.</h3>
+                     <p>2018</p>
+                     <p>Pencil on paper then digially colored in photoshop.</p>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+           <div className="row-4">
+             <div className="responsive">
+               <div className="gallery">
+                 <div className="flip-card">
+                 <div className="flip-card-inner">
+                     <div className="flip-card-front">
+                       <img src="images/drawing-1-2018.png" alt="draw" width=  "550" height= "300"/>
+                     </div>
+                     <div className="flip-card-back">
+                       <h3>Leo and Sonic eating Lunch from Rocket Burger's in front of down town NYC.</h3>
+                       <p>2017/2018</p>
+                       <p>Pencil on paper then digially colored in photoshop and photo taken by me too.</p>
+                     </div>
+                   </div>
+                 </div>
+               </div>
+             </div>
+             <div className="responsive">
+               <div className="gallery">
+                 <div className="flip-card">
+                   <div className="flip-card-inner">
+                     <div className="flip-card-front">
+                       <img src="images/drawing-10-2018.png" alt="draw" width=  "450" height= "600"/>
+                     </div>
+                     <div className="flip-card-back">
+                       <h3>Notebook Samurai</h3>
+                       <p>2017/2018</p>
+                       <p>Pencil on paper then digially colored in photoshop. Photo taken by me.</p>
+                     </div>
+                   </div>
+                 </div>
+               </div>
+             </div>
+             <div className="responsive">
+               <div className="gallery">
+                 <div className="flip-card">
+                   <div className="flip-card-inner">
+                     <div className="flip-card-front">
+                       <img src="images/drawing-5-2018.png" alt="draw" width=  "450" height= "600"/>
+                     </div>
+                     <div className="flip-card-back">
+                       <h3>Fooly Cooly</h3>
+                       <p>2017/2018</p>
+                       <p>Pencil on paper then digially colored in photoshop.</p>
+                     </div>
+                   </div>
+                 </div>
+               </div>
+             </div>
+         </div>
+         <div className="row-1">
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+                 <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/drawing-9-2018.png" alt="draw" width=  "400" height= "600"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>Pink Diamond reveals her plan to Pearl.</h3>
+                     <p>2017/2018</p>
+                    <p>Pencil on paper then digially colored in photoshop.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+           </div>
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+               <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/drawing-11-2018.png" alt="draw" width=  "550" height= "300"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>Shiny Chariot flying though the night sky. From Litte Witch Academia</h3>
+                     <p>2017/2018</p>
+                     <p>Pencil on paper then digially colored in photoshop. Photo taken by me.</p>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+               <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/drawing-8-2017.JPG" alt="draw" width=  "550" height= "300"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>Luffy taking a nap.</h3>
+                     <p>2017</p>
+                     <p>Pencil on paper</p>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+         <div className="row-1">
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+                 <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/desgin-May-I-shoot-you.png" alt="design" width=  "400" height= "600"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>T-shirt concept</h3>
+                     <p>2015</p>
+                    <p>Adobe photoshop and Photograph of my Eastman Kodak camera</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+           </div>
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+                 <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/design-logo-summer-bike.png" alt="art" width=  "400" height= "600"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>End of Summer Logo</h3>
+                     <p>2016</p>
+                    <p>Adobe Photoshop</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+           </div>
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+                 <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/design-dunkin.jpg" alt="design" width=  "500" height= "300"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>Sales Flyer</h3>
+                     <p>2016</p>
+                     <p>Adobe Indesign. Done for the Real Estate company <a href= "http://www.southeastrealtygroup.net/index.html">South East Realty Group</a>. I created there Website for the company </p>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+         <div className="row-1">
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+                 <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/design-1-2015.jpg" alt="design" width=  "400" height= "600"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>Shoe Store sale mail out</h3>
+                     <p>2015</p>
+                    <p>Adobe Indesign and photoshop. Done for the shoe store I was working at.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+           </div>
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+                 <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/art-color-fall-2.jpg" alt="art" width=  "400" height= "600"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>Color Fall 2</h3>
+                     <p>2015</p>
+                    <p>Photographed using Canon T6i Rebel and digially edited in photoshop</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+           </div>
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+                 <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/art-evening-sunset-2015.jpg" alt="art" width=  "500" height= "300"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>Evening Sunset</h3>
+                     <p>2015</p>
+                     <p>Photographed using Canon T6i Rebel and digially edited in photoshop</p>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+         <div className="row-1">
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+                 <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/clay-dog-pitcher-2013.jpg" alt="guitar" width=  "400" height= "600"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>Dog Pitcher</h3>
+                     <p>2013</p>
+                    <p>Clay fired at cone 10. Done for Wheel 2 college class.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+           </div>
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+                 <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/clay-ancient-arifact-2014.jpg" alt="clay" width=  "400" height= "600"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>Ancient Arifact</h3>
+                     <p>2014</p>
+                    <p>Clay fired at cone 10. Done for Hand Building 2 college class.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+           </div>
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+                 <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/clay-island-house-2014.jpg" alt="clay" width=  "500" height= "300"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>Island House</h3>
+                     <p>2014</p>
+                     <p>Clay fired at cone 10. Done for Hand Building 2 college class.</p>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+         <div className="row-4">
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+                 <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/painting-2-2013.JPG" alt="painting" width=  "400" height= "600"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>In to the Sea</h3>
+                     <p>2013</p>
+                    <p>Paint on paper, made using cut outs. Done for a drawing class.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+           </div>
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+                 <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/painting-1-2013.JPG" alt="guitar" width=  "400" height= "600"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>Colors that repersent dark and light</h3>
+                     <p>2012</p>
+                    <p>Paint on paper. Done for Color Fundmental</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+           </div>
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+                 <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/drawing-12-2012.jpg" alt="draw" width=  "500" height= "300"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>Erasing Drawings 1</h3>
+                     <p>2012</p>
+                     <p>Pencil on paper and gummy eraser. Done for a drawing class in college.</p>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+      </>
+   )
+  }
  }
-}
 
 export default Artarchive;
