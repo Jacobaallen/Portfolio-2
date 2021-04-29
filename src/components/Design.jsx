a4426b7807bbc12cf09df44dc154419ec8a561e5
jacob <boat.mon@gmail.com>
2021-04-28 22:56:37 -0400

    The new design page showing off examples of my design work

---
+++ src/components/Design.jsx
@@ -0,0 +1,339 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Gallery from 'react-grid-gallery';
+import '../styles/design.css';
+
+class Artarchive extends React.Component {
+ render() {
+   return (
+     <>
+     <div className="bgimage-2">
+        <div className="transbox">
+          <div className="portfolio">
+            <p>Design</p>
+          </div>
+        </div>
+      </div>
+
+        <div className="row-4">
+          <div className="responsive">
+            <div className="gallery">
+              <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <img src="images/afh-meme-logo.png" alt="draw" width=  "550" height= "300"/>
+                </div>
+                <div className="flip-card-back">
+                  <h3>AFH Meme Logo</h3>
+                  <p>2021</p>
+                  <p>AFH Meme is a suvsidiay of Anime Fan Helper. It is account on Instagram</p>
+                  <p><a href="https://www.instagram.com/afh_meme/">AFH Meme on Instagram</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+          <div className="responsive">
+            <div className="gallery">
+              <div className="flip-card">
+              <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    <img src="images/News-stories-2.png" alt="draw" width=  "550" height= "300"/>
+                  </div>
+                  <div className="flip-card-back">
+                    <h3>Anime Fan Helper News Design</h3>
+                    <p>2019- ongoing</p>
+                    <p><a href="http://www.animefanhelper.com/index.html">Anime Fan Helper</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="responsive">
+            <div className="gallery">
+              <div className="flip-card">
+              <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    <img src="images/afh-weeky-anime-news.png" alt="photo" width=  "550" height= "300"/>
+                  </div>
+                  <div className="flip-card-back">
+                    <h3>AFH: Weeky Anime News</h3>
+                    <p>2020</p>
+                    <p>Adobe Photoshop and Imovie</p>
+                    <p><a href="https://www.youtube.com/channel/UCBblK7pR1qxbu4bQ7ReyNQA/featured">Created for Anime Fan Helper as a weekly anime news series.You find the link to all videos by clicking here.</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row-1">
+          <div className="responsive">
+            <div className="gallery">
+              <div className="flip-card">
+                <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    <img src="images/x1law.png" alt="cards" width=  "550" height= "300"/>
+                  </div>
+                  <div className="flip-card-back">
+                    <h3>X1 Law Hero Images</h3>
+                    <p>2020 - ongoing</p>
+                    <p>Photoshop</p>
+                    <p><a href="http://x1law.com">Created for the website X1law</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="responsive">
+            <div className="gallery">
+              <div className="flip-card">
+                <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    <img src="images/compass.png" alt="draw" width=  "550" height= "300"/>
+                  </div>
+                  <div className="flip-card-back">
+                    <h3>Southeast Realty Group Inc. new logo</h3>
+                    <p>2019</p>
+                    <p>Photoshop</p>
+                    <p><a href="http://www.southeastrealtygroup.net/index.html">Currently used on southeastrealtygroup.net</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="responsive">
+            <div className="gallery">
+              <div className="flip-card">
+                <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    <img src="images/Anime-fan-helper-logo.png" alt="draw" width=  "550" height= "300"/>
+                  </div>
+                  <div className="flip-card-back">
+                    <h3>Anime Fan Helper logo</h3>
+                    <p>2019</p>
+                    <p>Digially made in Photoshop.</p>
+                    <p>Currently used on my website</p>
+                    <p><a href="http://www.animefanhelper.com/index.html">Anime Fan Helper</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row-1">
+          <div className="responsive">
+            <div className="gallery">
+              <div className="flip-card">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                    <img src="images/drawing-7-2019.png" alt="draw" width=  "550" height= "300"/>
+                  </div>
+                  <div className="flip-card-back">
+                    <h3>Concept T-shirt Design Enchanting Anime Fan Helper</h3>
+                    <p>2019</p>
+                    <p>Digially drawn on Ipad in Adobe Draw, edited in photoshop and photographed</p>
+                    <p>Designed as concepts for a client who never responded to my emails or calls after the first meeting</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="responsive">
+            <div className="gallery">
+              <div className="flip-card">
+                <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    <img src="images/tshirt-concept-design.png" alt="draw" width=  "450" height= "600"/>
+                  </div>
+                  <div className="flip-card-back">
+                    <h3>T-shirt Concept Design</h3>
+                    <p>2019</p>
+                    <p>Pencil on paper, Ipad Draw, Adobe Photoshop, Adobe Illustrator</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="responsive">
+            <div className="gallery">
+              <div className="flip-card">
+              <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    <img src="images/logo-green-bird-cafe.png" alt="logo" width=  "550" height= "300"/>
+                  </div>
+                  <div className="flip-card-back">
+                    <h3>Green's Bird Cafe Logo</h3>
+                    <p>2017</p>
+                    <p>Adobe Photoshop, Iphone camera</p>
+                    <p><a href="https://youtu.be/FzepQ-NaV7E">The logo was designed in part of a fake Commercial for my Multi Media Design Class at Palm Beach State College. Click here to see the video I made.</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+         <div className="row-4">
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+               <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/flyers-group.png" alt="flyers" width=  "550" height= "300"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>Jacob A. Allen Photography & Graphic Design Flyers</h3>
+                     <p>2015 - 2016</p>
+                     <p>Adobe Photoshop and Adobe Indesign</p>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+                 <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/jaa-logo.png" alt="design" width=  "400" height= "600"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>Jacob A. Allen Logo</h3>
+                     <p>2016</p>
+                    <p>Adobe photoshop</p>
+                    <p>Designed to replace my old logo. This has been my personal logo since 2016</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+           </div>
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+                 <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                   <img src="images/design-dunkin.jpg" alt="design" width=  "500" height= "300"/>
+                 </div>
+                 <div className="flip-card-back">
+                   <h3>Sales Flyer</h3>
+                   <p>2016</p>
+                   <p>Adobe Indesign. Done for the Real Estate company <a href= "http://www.southeastrealtygroup.net/index.html">Southeast Realty Group</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+           </div>
+         </div>
+         <div className="row-4">
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+                 <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/concert-poster-and-ticket.png" alt="design" width=  "500" height= "300"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>Fake Reel Big Fish Poster & Ticket</h3>
+                     <p>2016</p>
+                     <p>Adobe Indesign and Adobe Photoshop CC</p>
+                     <p>Was a project for my publication design class.</p>
+                     <p>The project was to desgin a concert poster and ticket.</p>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+                 <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/magazine-cover-and-centerfold.png" alt="design" width=  "400" height= "600"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>Fake Magazine Cover & Centerfold</h3>
+                     <p>2016</p>
+                    <p>Adobe Indesign, Adobe Photoshop CC, & Canon camera</p>
+                    <p> Was a project for my publication design class.</p>
+                    <p> The project was to design a magazine cover and centerfold.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+           </div>
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+                <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                  <img src="images/design-logo-summer-bike.png" alt="art" width=  "400" height= "600"/>
+                </div>
+                <div className="flip-card-back">
+                    <h3>End of Summer Logo</h3>
+                    <p>2016</p>
+                    <p>Adobe Photoshop</p>
+                    <p>Created as part of a Web Design class final project. I made the website as well. <a href="https://tourdeendofsummer.herokuapp.com/index.html">Tour De End of Summer</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+           </div>
+         </div>
+         <div className="row-3">
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+                 <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/sara-and-marcy-posters.png" alt="design" width=  "500" height= "300"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>Sara and Marcy Jewlery Posters and Flyers</h3>
+                     <p>2015-2017</p>
+                     <p>Adobe Indesign and Adobe Photoshop</p>
+                     <p>A collection of repetitive examples created to created a band image of a small jewelry sale business.</p>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+                 <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/postcards-group.png" alt="design" width=  "400" height= "600"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>Shoe Store Sale Mail Outs</h3>
+                     <p>2015</p>
+                    <p>Adobe Indesign and photoshop</p>
+                    <p>Representative Example of work done</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+           </div>
+           <div className="responsive">
+             <div className="gallery">
+               <div className="flip-card">
+                 <div className="flip-card-inner">
+                   <div className="flip-card-front">
+                     <img src="images/t-and-b.png" alt="photo" width=  "400" height= "600"/>
+                   </div>
+                   <div className="flip-card-back">
+                     <h3>Tech and Bowery Life Style Photography</h3>
+                     <p>2014</p>
+                     <p>Canon Rebel T3 and Adobe Photoshop</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+           </div>
+         </div>
+    </>
+   )
+  }
+ }
+
+export default Artarchive;
